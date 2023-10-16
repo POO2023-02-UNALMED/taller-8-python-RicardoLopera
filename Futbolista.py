51edb0a9499a4466f195d06431b899f36821ffe4
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-10-16 05:16:07 -0500

    ale mijo

---
+++ Futbolista.py
@@ -0,0 +1,44 @@
+from persona import Persona
+from deportista import Deportista
+
+class Futbolista(Persona,Deportista):
+    listaFutbolistas = []
+
+    def __init__(self, nombre, edad, altura, sexo, anosPracticando,golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(self,nombre, edad, altura, sexo)
+        Deportista.__init__(self, anosPracticando)
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._piernaHabil = piernaHabil
+        Futbolista.listaFutbolistas.append(self)
+    
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+    
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+
+    @classmethod
+    def getListaFutbolistas(clc):
+        return clc.listaFutbolistas
+    
+    @classmethod
+    def setListaFutbolistas(clc, listaFutbolistas):
+        clc.listaFutbolistas = listaFutbolistas
+
+    def __str__(self):
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte 
+        {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo 
+        {self.getAñosPracticando()} años en el deporte"
---
+++ deportista.py
@@ -0,0 +1,17 @@
+class Deportista(object):
+    def __init__(self, deporte, anosPracticando):
+        self._deporte = "Futbol"
+        self._anosPracticando = anosPracticando
+    
+    def getDeporte(self):
+        return self._deporte
+    
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getAnosPracticando(self):
+        return self._anosPracticando
+    
+    def setAnorPracticando(self, anosPracticando):
+        self._anosPracticando = anosPracticando
+
